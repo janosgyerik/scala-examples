07c542faae117e906c1b37ca93efb6ac7e143f2e
Janos Gyerik <janos@kronos>
2014-05-29 19:52:50 +0200

    added pascal triangle implementation

---
+++ scala-examples/src/main/scala/com/janosgyerik/scala/Pascal.scala
@@ -0,0 +1,16 @@
+package com.janosgyerik.scala
+
+object Pascal {
+  def pascal(c: Int, r: Int): Int = {
+    require(!(c < 0 || r < 0 || c > r), "c, r must be both positive and c <= r")
+    if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
+  def main(args: Array[String]) {
+	println(pascal(3, 4))
+	println(pascal(3, 3))
+	//println(pascal(4, 3))
+  }
+}
