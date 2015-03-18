d804787830368241ebab8cec4e47f4fd524966c2
Janos Gyerik <janos@kronos>
2015-03-18 20:43:24 +0100

    added some examples

--- scala-examples/src/main/scala/com/janosgyerik/scala/HelloWorld.scala
+++ scala-examples/src/main/scala/com/janosgyerik/scala/HelloWorld.scala
@@ -5,3 +5,22 @@ object HelloWorld {
     println("Hello, world!")
   }
 }
+/*
+ * scala> com.example.scala.HelloWorld.main(Array())
+Hello, world!
+
+scala> Range(1,5)
+* 
+* 
+scala> com.example.scala.HelloWorld.main(Array())
+Hello, world!
+
+scala> Range(1,5)
+res5: scala.collection.immutable.Range = Range(1, 2, 3, 4)
+
+scala> Range(1, 10).filter(_ % 3 == 0)
+res6: scala.collection.immutable.IndexedSeq[Int] = Vector(3, 6, 9)
+
+scala> res6.map(x => 3 * x + 1)
+res7: scala.collection.immutable.IndexedSeq[Int] = Vector(10, 19, 28)
+*/
