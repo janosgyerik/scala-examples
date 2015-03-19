17cafd75d6c8869ec032da422cf03240bf040764
Janos Gyerik <janos@kronos>
2015-03-19 08:01:05 +0100

    rewrote SquareSum using proper dp

--- scala-examples/src/main/scala/com/janosgyerik/scala/practice/SquareSum.scala
+++ scala-examples/src/main/scala/com/janosgyerik/scala/practice/SquareSum.scala
@@ -2,17 +2,19 @@ package com.janosgyerik.scala.practice
 
 object SquareSum {
 
-  def squaresUpTo(n: Int) = 1.to(n).filter(x => x * x <= n).map(x => x * x)
+  def squaresUpTo(n: Int): List[Int] = (1 to n).filter(x => x * x <= n).map(x => x * x).toList
 
-  def minSquaresSummingTo(n: Int): List[Int] = {
-    val squares = squaresUpTo(n)
-
-    def inner(n: Int): List[Int] = {
-      if (n < 1) List()
-      else if (n == 1) List(1)
-      else squares.filter(x => x <= n).map(x => List(x) ++ inner(n - x)).minBy(list => list.size).sorted
+  def minSquaresSummingTo(n: Int): Int = {
+    def inner(minTermCount: List[Int], squares: List[Int]): Int = squares match {
+      case List() => minTermCount.last
+      case _ =>
+        val sq = squares.head
+        val newMinTermCount = (0 to n).map(x => Math.min(minTermCount(x), x / sq + minTermCount(x % sq))).toList
+        inner(newMinTermCount, squares drop 1)
     }
-    inner(n)
+    val minTermCount = (0 to n).toList
+    val squares = squaresUpTo(n) drop 1
+    inner(minTermCount, squares)
   }
 
 }
--- scala-examples/src/test/scala/com/janosgyerik/scala/practice/SquareSumTest.scala
+++ scala-examples/src/test/scala/com/janosgyerik/scala/practice/SquareSumTest.scala
@@ -10,35 +10,34 @@ class SquareSumTest extends FunSuite {
   import com.janosgyerik.scala.practice.SquareSum._
 
   test("min squares that sum to 1") {
-    assert(List(1) == minSquaresSummingTo(1))
+    assert(List(1).size == minSquaresSummingTo(1))
   }
 
   test("min squares that sum to 4") {
-    assert(List(4) == minSquaresSummingTo(4))
+    assert(List(4).size == minSquaresSummingTo(4))
   }
 
   test("min squares that sum to 11 = 1 + 1 + 9") {
-    assert(List(1, 1, 9) == minSquaresSummingTo(11))
+    assert(List(1, 1, 9).size == minSquaresSummingTo(11))
   }
 
   test("min squares that sum to 12 = 4 + 4 + 4") {
-    assert(List(4, 4, 4) == minSquaresSummingTo(12))
+    assert(List(4, 4, 4).size == minSquaresSummingTo(12))
   }
 
   test("min squares that sum to 13 = 4 + 9") {
-    assert(List(4, 9) == minSquaresSummingTo(13))
+    assert(List(4, 9).size == minSquaresSummingTo(13))
   }
 
   test("min squares that sum to 35") {
-    assert(List(1, 16, 16) == minSquaresSummingTo(33))
+    assert(List(1, 16, 16).size == minSquaresSummingTo(33))
   }
 
   test("min squares that sum to 40") {
-    assert(List(4, 36) == minSquaresSummingTo(40))
+    assert(List(4, 36).size == minSquaresSummingTo(40))
   }
 
-//  TODO: very slow!
-//  test("min squares that sum to 49") {
-//    assert(List(49) == minSquaresSummingTo(49))
-//  }
+  test("min squares that sum to 49") {
+    assert(List(49).size == minSquaresSummingTo(49))
+  }
 }
