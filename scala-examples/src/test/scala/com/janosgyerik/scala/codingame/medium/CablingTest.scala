632c8a30f08fd3bc2861fc6258a7d347732fce57
Janos Gyerik <janos@kronos>
2015-04-04 23:37:20 +0200

    added more test cases

--- scala-examples/src/test/scala/com/janosgyerik/scala/codingame/medium/CablingTest.scala
+++ scala-examples/src/test/scala/com/janosgyerik/scala/codingame/medium/CablingTest.scala
@@ -19,6 +19,18 @@ class CablingTest extends FunSuite {
 
   val input1 = Array("3", "0 0", "1 1", "2 2")
   val input3 = Array("4", "1 2", "0 0", "2 2", "1 3")
+  val input4 = Array("1", "1 1")
+  val input5 = Array("3", "-5 -3", "9 2", "3 -4")
+  val input6 = Array("8",
+    "-28189131 593661218",
+    "102460950 1038903636",
+    "938059973 -816049599",
+    "-334087877 -290840615",
+    "842560881 -116496866",
+    "-416604701 690825290",
+    "19715507 470868309",
+    "846505116 -694479954"
+  )
 
   test("parseInput 3, 0 0, 1 1, 2 2") {
     assert(List((0, 0), (1, 1), (2, 2)) == parseInput(input1.toIterable))
@@ -82,5 +94,17 @@ class CablingTest extends FunSuite {
 
   test("minLength, input3") {
     assert(5 == minLength(parseInput(input3)))
+  }
+
+  test("minLength, input4") {
+    assert(0 == minLength(parseInput(input4)))
+  }
+
+  test("minLength, input5") {
+    assert(18 == minLength(parseInput(input5)))
+  }
+
+  test("minLength, input6") {
+    assert(6066790161L == minLength(parseInput(input6)))
   }
 }
