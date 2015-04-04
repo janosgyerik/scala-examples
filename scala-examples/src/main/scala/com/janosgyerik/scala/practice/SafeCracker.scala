d9539453e51a281bf51337604bac05ad3399dc1d
Janos Gyerik <janos@kronos>
2015-04-04 23:03:08 +0200

    safecracker progress

--- scala-examples/src/main/scala/com/janosgyerik/scala/practice/SafeCracker.scala
+++ scala-examples/src/main/scala/com/janosgyerik/scala/practice/SafeCracker.scala
@@ -7,9 +7,20 @@ object SafeCracker {
 
   def cracker(seq: String) = {
     seq
+    // while set.size of unique combinations is < 10^digits
+    // find a shortest suitable sequence to append
+    // for toAppendLength in (1 to digits)
+    //  iter = perm(toAppendLength)
+    //  prefix = seq.takeRight(digits - toAppendLength)
+    //  while iter.hasNext
+    //    next = iter.next
+    //    candidate = prefix + next
+    //    try if this is a new combination: candidate
+    //      if yes: return f(unique + candidate, seq + next)
   }
 
   def combinations(prefix: String) = {
+    prefix.toList.map(x => x - '0')
   }
 
   def perm(n: Int): List[List[Int]] = n match {
