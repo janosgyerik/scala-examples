be1a4bc06bdb261f5a7c4967a3afaa663a1670bb
Janos Gyerik <janos@kronos>
2015-04-13 22:15:22 +0200

    full rewrite of state machine in progress

--- scala-examples/src/main/scala/com/janosgyerik/scala/codingame/easy/SkynetChasm.scala
+++ scala-examples/src/main/scala/com/janosgyerik/scala/codingame/easy/SkynetChasm.scala
@@ -4,66 +4,99 @@ object SkynetChasm {
 
   abstract class Action
 
-  case class Speed() extends Action
-  case class Wait() extends Action
-  case class Slow() extends Action
-  case class Jump() extends Action
+  case object Speed extends Action
+  case object Wait extends Action
+  case object Slow extends Action
+  case object Jump extends Action
 
-  class GameParams(val initialSpeed: Int, val gapStart: Int, val landingStart: Int, val landingEnd: Int)
-
-  class BikeState(val speed: Int, val pos: Int = 0, val landed: Boolean = false, val alive: Boolean = true)
-
-  def applyAction(params: GameParams, state: BikeState, action: Action) = {
-    val newSpeed = action match {
-      case Speed() => state.speed + 1
-      case Slow() => state.speed - 1
-      case _ => state.speed
-    }
-
-    val newPos = state.pos + newSpeed
-
-    val onLanding = newPos >= params.landingStart && newPos <= params.landingEnd
-
-    val landed = onLanding && newSpeed == 0
-
-    val alive = onLanding || newPos < params.gapStart
-
-    new BikeState(newSpeed, newPos, landed, alive)
+  trait State {
+    def next(action: Action): State = throw new IllegalAccessError("This is a terminating state")
   }
 
-  def findSuccessfulActionSequence(params: GameParams): List[Action] = {
-    def findSuccessfulActionSequence(list: List[Action], state: BikeState): List[Action] = {
-      if (state.landed) list
-      else if (!state.alive) List()
-      else findSuccessfulActionSequenceFromState(list, state)
-    }
+  case object Dead extends State
+  case object Landed extends State
 
-    def findSuccessfulActionSequenceFromState(list: List[Action], state: BikeState): List[Action] = {
-      val possibleActions = getPossibleActions(params, state)
-      var index = 0
-      while (index < possibleActions.size) {
-        val action = possibleActions(index)
-        val newState = applyAction(params, state, action)
-        val successfulActionSequence = findSuccessfulActionSequence(action :: list, newState)
-        if (successfulActionSequence.nonEmpty) return successfulActionSequence.reverse
-        index = index + 1
+  case class Running(game: Game, speed: Int, pos: Int = 0) extends State {
+    require(posOnRunway(pos) || posOnLanding(pos))
+
+    def posOnRunway(pos: Int) =
+      0 <= pos && pos < game.gapStart
+
+    def posOnLanding(pos: Int) =
+      game.landingStart <= pos && pos < game.landingEnd
+
+    override def next(action: Action): State = {
+      val newSpeed = action match {
+        case Speed => speed + 1
+        case Slow => speed - 1
+        case _ => speed
       }
-      List()
+
+      val newPos = pos + newSpeed
+
+      val wasOnRunway = posOnRunway(pos)
+      val wasOnLanding = posOnLanding(pos)
+      val onRunway = posOnRunway(newPos)
+      val onLanding = posOnLanding(newPos)
+      val stopped = newSpeed == 0
+      val onLandingSlowingDown = wasOnLanding && onLanding && !stopped
+      val jumpedOverGap = wasOnRunway && onLanding && action == Jump
+
+      if (onRunway || onLandingSlowingDown || jumpedOverGap) Running(game, newSpeed, newPos)
+      else if (onLanding && stopped) Landed
+      else Dead
     }
-
-    val initialState = getInitialState(params)
-
-    findSuccessfulActionSequenceFromState(List(), initialState)
   }
 
-  def getPossibleActions(params: GameParams, state: BikeState) = {
-    if (state.pos < params.gapStart) {
-      if (state.speed == 0) List(Speed())
-      else if (state.pos + state.speed >= params.gapStart) List(Jump())
-      else List(Speed(), Slow(), Wait(), Jump())
-    }
-    else List(Speed(), Slow(), Wait(), Jump())
+  class Game(val initialSpeed: Int, val gapStart: Int, val landingStart: Int, val landingEnd: Int) {
+    val initialState: State = Running(this, initialSpeed)
   }
 
-  def getInitialState(params: GameParams) = new BikeState(params.initialSpeed)
+//  def findSuccessfulActionSequence(params: GameParams): List[Action] = {
+//    def findSuccessfulActionSequence(list: List[Action], state: BikeState): List[Action] = {
+//      if (state.landed) list
+//      else if (!state.alive) List()
+//      else findSuccessfulActionSequenceFromState(list, state)
+//    }
+//
+//    def findSuccessfulActionSequenceFromState(list: List[Action], state: BikeState): List[Action] = {
+//      val possibleActions = getPossibleActions(params, state)
+//      var index = 0
+//      while (index < possibleActions.size) {
+//        val action = possibleActions(index)
+//        val newState = applyAction(params, state, action)
+//        val successfulActionSequence = findSuccessfulActionSequence(action :: list, newState)
+//        if (successfulActionSequence.nonEmpty) return successfulActionSequence.reverse
+//        index = index + 1
+//      }
+//      List()
+//    }
+//
+//    val initialState = getInitialState(params)
+//
+//    findSuccessfulActionSequenceFromState(List(), initialState)
+//  }
+//
+//  def isBeforeGap(params: GameParams, state: BikeState): Boolean =
+//    state.pos < params.gapStart
+//
+//  def isGapImminent(params: GameParams, state: BikeState): Boolean =
+//    state.pos + state.speed >= params.gapStart
+//
+//  def getCommonReasonableActions(state: BikeState): List[Action] = {
+//    List(Speed(), Wait()) ++ { if (state.speed > 1) List(Slow()) else Nil }
+//  }
+//
+//  def getPossibleActions(params: GameParams, state: BikeState): List[Action] = {
+//    if (isBeforeGap(params, state)) {
+//      if (state.speed == 0) List(Speed())
+//      else if (isGapImminent(params, state)) List(Jump())
+//      else getCommonReasonableActions(state)
+//    } else {
+////      List(Slow())
+//      getCommonReasonableActions(state)
+//    }
+//  }
+//
+//  def getInitialState(params: GameParams) = new BikeState(params.initialSpeed)
 }
--- scala-examples/src/test/scala/com/janosgyerik/scala/codingame/easy/SkynetChasmTest.scala
+++ scala-examples/src/test/scala/com/janosgyerik/scala/codingame/easy/SkynetChasmTest.scala
@@ -10,32 +10,72 @@ class SkynetChasmTest extends FunSuite {
 
   import com.janosgyerik.scala.codingame.easy.SkynetChasm._
 
-  val params = new GameParams(0, 20, 22, 100)
+  val gapStart = 30
+  val landingStart = 35
+  val landingEnd = 100
+  val game = new Game(0, gapStart, landingStart, landingEnd)
 
-  test("initial state") {
-    val state = getInitialState(params)
-    assert(0 == state.pos)
-    assert(params.initialSpeed == state.speed)
-    assert(state.alive)
-    assert(!state.landed)
+  test("Running: invalid if before landing start") {
+    intercept[IllegalArgumentException](Running(game, 0, -1))
   }
 
-  test("apply action Speed at pos 0") {
-    val state = getInitialState(params)
-    val newState = applyAction(params, state, Speed())
-    assert(state.speed + 1 == newState.speed)
+  test("Running: invalid if after landing") {
+    intercept[IllegalArgumentException](Running(game, 0, landingEnd + 1))
   }
 
-  test("possible action at speed 0 before gap is Speed") {
-    assert(List(Speed()) == getPossibleActions(params, new BikeState(0, 0)))
+  test("Running: invalid if on gap start") {
+    intercept[IllegalArgumentException](Running(game, 0, gapStart))
   }
 
-  test("possible action at gap - speed is Jump") {
-    assert(List(Jump()) == getPossibleActions(params, new BikeState(1, params.gapStart - 1)))
+  test("Running: invalid if in the gap") {
+    intercept[IllegalArgumentException](Running(game, 0, gapStart + 1))
+  }
+
+  test("Running: invalid if right before landing start") {
+    intercept[IllegalArgumentException](Running(game, 0, landingStart - 1))
+  }
+
+  test("Running: valid sanity test") {
+    Running(game, 0)
+  }
+
+  test("Dead: if fail to jump when gap is imminent") {
+    val state = Running(game, landingStart - gapStart + 1, gapStart - 1)
+    assert(Dead === state.next(Speed))
+    assert(Dead === state.next(Wait))
+    assert(Dead === state.next(Slow))
+    assert(Dead !== state.next(Jump))
+  }
+
+  test("Dead: in front of the gap and too slow") {
+    val state = Running(game, landingStart - gapStart, gapStart - 1)
+    assert(Dead === state.next(Speed))
+    assert(Dead === state.next(Wait))
+    assert(Dead === state.next(Slow))
+    assert(Dead === state.next(Jump))
+  }
+
+  test("Landed: on landing and stopped") {
+    val state = Running(game, 1, landingStart)
+    assert(Landed === state.next(Slow))
+  }
+
+  test("Landed: on landing and slow down in 2 steps") {
+    val state = Running(game, 2, landingStart)
+    assert(Landed !== state.next(Slow))
+    assert(Landed === state.next(Slow).next(Slow))
+  }
+
+  test("Dead: on landing but too fast to stop") {
+    val state = Running(game, 5, landingEnd - 10)
+    assert(Dead !== state.next(Slow))
+    assert(Dead !== state.next(Slow).next(Slow))
+    assert(Dead !== state.next(Slow).next(Slow).next(Slow))
+    assert(Dead === state.next(Slow).next(Slow).next(Slow).next(Slow))
   }
 
   test("successful action sequence") {
-    val sequence = findSuccessfulActionSequence(params)
-//    assert("" == sequence.toString())
+    //    val sequence = findSuccessfulActionSequence(params)
+    //    assert("" == sequence.toString())
   }
 }
