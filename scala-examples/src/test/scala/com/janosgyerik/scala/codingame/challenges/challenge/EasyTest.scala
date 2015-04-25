ccb5bd8d0db253b753a53d0a807f95a07c0fb6b0
Janos Gyerik <janos@kronos>
2015-04-25 21:12:53 +0200

    moved common test data to outer scope

--- scala-examples/src/test/scala/com/janosgyerik/scala/codingame/challenges/challenge/EasyTest.scala
+++ scala-examples/src/test/scala/com/janosgyerik/scala/codingame/challenges/challenge/EasyTest.scala
@@ -10,43 +10,28 @@ class EasyTest extends FunSuite {
 
   import Solution._
 
-  test("parseInput 1.2 ... ..1") {
-    val input = Array("1.2", "...", "..1")
+  val input = Array("1.2", "...", "..1")
+  val links = parseLinks(input)
+  val nodes = getNodes(links)
 
-    val links = parseLinks(input)
+  test("parseInput example1") {
     assert("List(Link(Node(0,0,1),Node(2,0,2)), Link(Node(2,0,2),Node(0,0,1)), Link(Node(2,0,2),Node(2,2,1)), Link(Node(2,2,1),Node(2,0,2)))"
       == links.toString)
 
     assert(Set(Node(0, 0, 1), Node(2, 2, 1), Node(2, 0, 2)) == getNodes(links))
   }
 
-  test("findNeighbors 1.2 ... ..1") {
-    val input = Array("1.2", "...", "..1")
-
-    val links = parseLinks(input)
-    val nodes = getNodes(links)
-
+  test("findNeighbors example1") {
     assert(Set(Node(2, 0, 2)) == nodes.head.findNeighbors(links))
-
     assert(Set(Node(0, 0, 1), Node(2, 2, 1)) == Node(2, 0, 2).findNeighbors(links))
   }
 
-  test("findNodesThatNeedAllPossibleConnections 1.2 ... ..1") {
-    val input = Array("1.2", "...", "..1")
-
-    val links = parseLinks(input)
-    val nodes = getNodes(links)
-
+  test("findNodesThatNeedAllPossibleConnections example1") {
     val game = new GameState(links, nodes)
     assert(Set(Node(2, 0, 2)) == game.findNodesThatNeedAllPossibleConnections)
   }
 
-  test("getAllPossibleConnections 1.2 ... ..1") {
-    val input = Array("1.2", "...", "..1")
-
-    val links = parseLinks(input)
-    val nodes = getNodes(links)
-
+  test("getAllPossibleConnections example1") {
     val game = new GameState(links, nodes)
     assert(Set((Link(Node(2,0,2),Node(0,0,1)),1), (Link(Node(2,0,2),Node(2,2,1)),1))
       == game.getAllPossibleConnections(Node(2, 0, 2)))
