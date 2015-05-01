d494d5db44456f9282430324ac658f5ec43c3e8f
Janos Gyerik <janos@kronos>
2015-05-01 23:35:21 +0200

    apu improvement wip

---
+++ scala-examples/src/main/scala/com/janosgyerik/scala/codingame/challenges/nospoon2/try3/ApuImprovement.scala
@@ -0,0 +1,134 @@
+package com.janosgyerik.scala.codingame.challenges.nospoon2.try3
+
+import java.util.Scanner
+
+object Player extends App {
+  val scanner = new Scanner(System.in)
+
+  solve(scanner)
+
+  def solve(scanner: Scanner): Unit = {
+    val width = scanner.nextInt
+    val height = scanner.nextInt
+    scanner.nextLine
+
+    val lines = {
+      for {_ <- List.range(0, height)} yield scanner.nextLine
+    }.toArray
+
+    import GameState._
+
+    val (nodes, neighbors, needs) = parseNodes(lines)
+    getOptimalConnections(nodes, neighbors, needs).foreach(println)
+  }
+}
+
+case class Node(rowNum: Int, colNum: Int)
+
+case class Conn(n1: Node, n2: Node, num: Int) {
+  override def toString: String = {
+    "%s %s %s %s %s".format(n1.colNum, n1.rowNum, n2.colNum, n2.rowNum, num)
+  }
+}
+
+object GameState {
+
+  val emptyMarker = '.'
+
+  def parseNodes(lines: Array[String]): (List[Node], Map[Node, List[Node]], Map[Node, Int]) = {
+    val width = lines(0).length
+    val height = lines.length
+    val End = Node(-1, -1)
+
+    def parseNodeWithNeed(rowNum: Int, colNum: Int) =
+      (Node(rowNum, colNum), lines(rowNum)(colNum) - '0')
+
+    def findRightNeighbor(node: Node) = {
+      val rowNum = node.rowNum
+      val row = lines(rowNum)
+
+      def findRightNeighbor(colNum: Int): Node = {
+        if (colNum >= width) End
+        else if (row(colNum) == emptyMarker) findRightNeighbor(colNum + 1)
+        else Node(rowNum, colNum)
+      }
+      findRightNeighbor(node.colNum + 1)
+    }
+
+    def findDownNeighbor(node: Node) = {
+      val colNum = node.colNum
+
+      def findDownNeighbor(rowNum: Int): Node = {
+        if (rowNum >= height) End
+        else if (lines(rowNum)(colNum) == emptyMarker) findDownNeighbor(rowNum + 1)
+        else Node(rowNum, colNum)
+      }
+      findDownNeighbor(node.rowNum + 1)
+    }
+
+    def parseNode(rowNum: Int, colNum: Int): (Node, Int, Node, Node) = {
+      val char = lines(rowNum)(colNum)
+      if (char == emptyMarker) (End, 0, End, End)
+      else {
+        val (node, need) = parseNodeWithNeed(rowNum, colNum)
+        val right = findRightNeighbor(node)
+        val down = findDownNeighbor(node)
+        (node, need, right, down)
+      }
+    }
+
+    val nodeInfoList = for {
+      rowNum <- List.range(0, lines.length)
+      colNum <- List.range(0, lines(0).length)
+      (node, need, right, down) = parseNode(rowNum, colNum)
+      if node != End
+    } yield (node, need, right, down)
+
+    val nodes = nodeInfoList.map(_._1)
+    val neighbors = nodeInfoList.groupBy(_._1).map {
+      case (node, (_, _, right, down)) => node -> List(right, down).filter(_ != End)
+    }
+    val needs = nodeInfoList.map { case (node, need, _) => node -> need }.toMap
+    (nodes, neighbors, needs)
+  }
+
+  def getOptimalConnections(nodes: List[Node], neighbors: Map[Node, List[Node]], needs: Map[Node, Int]): List[Conn] = {
+    getConnections(nodes, neighbors, needs)
+  }
+
+  def getNormalizedConnections(connections: List[Conn]) = {
+    connections.groupBy(_.n2).map {
+      case (n2, conns2) => n2 -> conns2.map(_.num).sum
+    }.map { case (n2, num) => Conn(connections.head.n1, n2, num) }.toList
+  }
+
+  def getConnections(nodes: List[Node], neighbors: Map[Node, List[Node]], needs: Map[Node, Int]): List[Conn] =
+    nodes match {
+      case Nil => Nil
+      case x :: xs =>
+        val connections = getNormalizedConnections(getConnections(x, neighbors, needs))
+        val updatedNeeds = getUpdatedNeeds(connections, needs)
+        connections ++ getConnections(xs, neighbors, updatedNeeds)
+    }
+
+  def getConnections(node: Node, neighbors: Map[Node, List[Node]], needs: Map[Node, Int]): List[Conn] = {
+    if (needs(node) == 0) Nil
+    else if (node.right.needs > 0) Conn(node, node.right) :: getConnections(takeFromRight(node))
+    else if (node.down.needs > 0) Conn(node, node.down) :: getConnections(takeFromDown(node))
+    else throw new IllegalStateException("no more available neighbors")
+  }
+
+  def takeFromRight(node: Node) =
+    Node(node.rowNum, node.colNum, node.needs - 1, node.right.satisfyOne, node.down)
+
+  def takeFromDown(node: Node) =
+    Node(node.rowNum, node.colNum, node.needs - 1, node.right, node.down.satisfyOne)
+
+  def getRest(nodes: List[Node], connections: List[Conn]): List[Node] =
+    for {
+      n1 <- nodes
+      conn <- connections
+      n2 = conn.n2
+      if n1.rowNum == n2.rowNum && n1.colNum == n2.colNum
+    } yield n1.satisfy(conn.num)
+}
---
+++ scala-examples/src/test/scala/com/janosgyerik/scala/codingame/challenges/nospoon2/try3/ApuImprovementTest.scala
@@ -0,0 +1,61 @@
+package com.janosgyerik.scala.codingame.challenges.nospoon2.try3
+
+import java.util.Scanner
+
+import com.janosgyerik.scala.codingame.challenges.nospoon2.level1.{GameState, Player}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ApuImprovementTest extends FunSuite {
+
+  import GameState._
+
+  val lines = Array("1.2", "...", "..1")
+  val nodes = parseNodes(lines)
+
+  test("example 1") {
+//    assert(isValidSolution(nodes, getOptimalConnections(nodes)))
+  }
+
+  test("normalized Conn") {
+    assert(Conn(Node(1, 2), Node(2, 3)) == Conn.create(Node(1, 2), Node(2, 3)))
+    assert(Conn(Node(1, 2), Node(2, 3)) == Conn.create(Node(2, 3), Node(1, 2)))
+  }
+
+  test("run example 1") {
+    Player.solve(new Scanner("3\n3\n1.2\n...\n..1\n"))
+  }
+
+  test("run example 2") {
+//    Player.solve(new Scanner("2\n2\n2.\n42\n"))
+  }
+
+  test("takeFromRight") {
+    val lines = Array("2.", "42")
+    val nodes = parseNodes(lines)
+    assert(Node(0,0,2,Node(-1,-1,0,null,null),Node(1,0,4,Node(-1,-1,0,null,null),Node(-1,-1,0,null,null)))
+      == nodes.head)
+    assert(Node(0,0,1,Node(-1,-1,0,null,null),Node(1,0,3,Node(-1,-1,0,null,null),Node(-1,-1,0,null,null)))
+      == takeFromDown(nodes.head))
+    assert(Node(0,0,0,Node(-1,-1,0,null,null),Node(1,0,2,Node(-1,-1,0,null,null),Node(-1,-1,0,null,null)))
+      == takeFromDown(takeFromDown(nodes.head)))
+  }
+
+  test("getNormalizedConnections") {
+    val conn1 = List(Conn(Node(1, 1), Node(2, 2), 1))
+    assert(conn1 == getNormalizedConnections(conn1))
+    assert(List(Conn(Node(1, 1), Node(2, 2), 3)) == getNormalizedConnections(conn1 ++ conn1 ++ conn1))
+  }
+
+  test("getConnections") {
+    val lines = Array("2.", "42")
+    val nodes = parseNodes(lines)
+    val x = nodes.head
+    val connections = getNormalizedConnections(getConnections(x))
+//    assert("" == connections)
+//    assert("" == getConnections(nodes))
+  }
+
+}
